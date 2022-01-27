b7bbef9984a53cc37c33814e90092827fc04fc60
Malhar Teli <malhar.teli@gmail.com>
2022-01-26 23:15:38 -0500

    Currency Api added

--- Assignments/Day003/calendar/src/main/java/com/example/calendar/CalendarApplication.java
+++ Assignments/Day003/calendar/src/main/java/com/example/calendar/CalendarApplication.java
@@ -1,13 +1,13 @@
-package com.example.customer;
+package com.example.calendar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CustomerApplication {
+public class CalendarApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CustomerApplication.class, args);
+		SpringApplication.run(CalendarApplication.class, args);
 
 	}
 
--- Assignments/Day003/currency/src/main/java/com/example/currency/CurrencyApplication.java
+++ Assignments/Day003/currency/src/main/java/com/example/currency/CurrencyApplication.java
@@ -1,13 +1,13 @@
-package com.example.customer;
+package com.example.currency;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CustomerApplication {
+public class CurrencyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CustomerApplication.class, args);
+		SpringApplication.run(CurrencyApplication.class, args);
 
 	}
 
