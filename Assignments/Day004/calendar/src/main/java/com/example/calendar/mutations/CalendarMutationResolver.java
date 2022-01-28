22d3f44a923b807b897b6cf2a735dd1875a78636
Malhar Teli <malhar.teli@gmail.com>
2022-01-28 00:47:27 -0500

    Weird issue with GraphQL refusing to recognize Schema

---
+++ Assignments/Day004/calendar/src/main/java/com/example/calendar/mutations/CalendarMutationResolver.java
@@ -0,0 +1,23 @@
+package com.example.calendar.mutations;
+
+import com.coxautodev.graphql.tools.GraphQLMutationResolver;
+import com.example.calendar.models.Calendar;
+import com.example.calendar.models.CalendarInput;
+import com.example.calendar.models.Country;
+import com.example.calendar.services.CalendarService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+
+@Component
+public class CalendarMutationResolver implements GraphQLMutationResolver {
+    @Autowired
+    private CalendarService calendarService;
+
+    public Calendar createCalendar(CalendarInput calendarInput){
+        Country countryObj = new Country(calendarInput.getCountryCode());
+        Calendar calendarObj = new Calendar(0, calendarInput.getEvent(), calendarInput.getCategory(), LocalDate.parse(calendarInput.getHolidayDate()), countryObj);
+        return this.calendarService.addCalendar(calendarObj);
+    }
+}
